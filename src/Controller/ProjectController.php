09aabdeff939d7312f40f72cee291dc64620e144
Dufour Jordan <jordandufour21@gmail.com>
2024-09-10 20:02:12 +0200

    Added project and rover templates, also updated home template

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProjectController extends AbstractController
+{
+    #[Route('/projects', name: 'app_project')]
+    public function index(): Response
+    {
+        return $this->render('project/index.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
+
+    #[Route('/projects/rover', name: 'app_rover')]
+    public function rover(): Response
+    {
+        return $this->render('project/rover.html.twig');
+    }
+}
